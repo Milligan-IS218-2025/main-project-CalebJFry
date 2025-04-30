4044b4de32c1e1639609645389f94b870e9f79a1
Caleb Fry <calebjfry2@gmail.com>
2025-04-30 03:14:42 -0400

    jh

--- form.js
+++ form.js
@@ -1,12 +1,13 @@
+//establish variables
 const form = document.getElementById("feedbackForm");
 const suggestionInput = document.getElementById("suggestion");
 const response = document.getElementById("response");
-
+//stops the page from reloading after hiting submit
 form.onsubmit = function(e) {
-  e.preventDefault(); // Prevent page reload
-
+  e.preventDefault();
+//reveals the hidden text thanking the user for their feedback
   if (suggestionInput.value.trim()) {
-    response.style.display = "block";  // Show thank-you message
-    suggestionInput.value = "";  // Clear the text area
+    response.style.display = "block";
+    suggestionInput.value = "";
   }
 };
